9656111b3d803f367c05dd58d47ac43632e8ecdd
Monky23 <lapaixjeanmichel@gmail.com>
2022-04-09 14:40:17 +0200

    views update

--- Views/blog/show.php
+++ Views/blog/show.php
@@ -1,8 +1,8 @@
 <h1><?= $params['post']->title ?></h1>
 <div>
     <?php foreach ($params['post']->getTags() as $tag) : ?>
-        <span class="badge badge-info"><?= $tag->name ?></span>
+        <span class="badge bg-info"><a href="/tags/<?= $tag->id ?>" class="text-white"><?= $tag->name ?></a></span>
     <?php endforeach ?>
 </div>
 <p><?= $params['post']->content ?></p>
-<a href="/posts" class="btn btn-secondary">Derniers articles</a>
+<a href="/posts" class="btn btn-secondary">Retourner en arrière</a>

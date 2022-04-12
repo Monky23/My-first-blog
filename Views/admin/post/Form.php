8b646526df7d0ee96c4ff93371387ad62d2c97b6
Monky23 <lapaixjeanmichel@gmail.com>
2022-04-12 20:02:39 +0200

    restore htmlspecialchars and replace intval by (int)

--- Views/admin/post/Form.php
+++ Views/admin/post/Form.php
@@ -1,25 +1,25 @@
-<h1><?= htmlentities($params['post']->title) ?? 'Créer un nouvel article' ?></h1>
+<h1><?= htmlspecialchars($params['post']->title) ?? 'Créer un nouvel article' ?></h1>
 
 <form action="<?= isset($params['post']) ? "/admin/posts/edit/{$params['post']->id}" : "/admin/posts/create" ?>" method="POST">
     <div class="form-group">
         <label for="title">Titre de l'article</label>
-        <input type="text" class="form-control" name="title" id="title" value="<?= htmlentities($params['post']->title) ?? '' ?>">
+        <input type="text" class="form-control" name="title" id="title" value="<?= htmlspecialchars($params['post']->title) ?? '' ?>">
     </div>
     <div class="form-group">
         <label for="content">Contenu de l'article</label>
-        <textarea name="content" id="content" rows="8" class="form-control"><?= htmlentities($params['post']->content) ?? '' ?></textarea>
+        <textarea name="content" id="content" rows="8" class="form-control"><?= htmlspecialchars($params['post']->content) ?? '' ?></textarea>
     </div>
     <div class="form-group">
         <label for="tags">Tags de l'article</label>
         <select multiple class="form-control" id="tags" name="tags[]">
             <?php foreach ($params['tags'] as $tag) : ?>
-                <option value="<?= intval($tag->id) ?>"
+                <option value="<?= (int)$tag->id ?>"
                 <?php if (isset($params['post'])) : ?>
                 <?php foreach ($params['post']->getTags() as $postTag) {
-                    echo (intval($tag->id) === intval($postTag->id)) ? 'selected' : '';
+                    echo ((int)$tag->id === (int)$postTag->id) ? 'selected' : '';
                 }
                 ?>
-                <?php endif ?>><?= htmlentities($tag->name) ?></option>
+                <?php endif ?>><?= htmlspecialchars($tag->name) ?></option>
             <?php endforeach ?>
         </select>
     </div>

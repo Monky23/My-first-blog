30d6337b7634311c8685d24f3519264f5577e93d
Monky23 <lapaixjeanmichel@gmail.com>
2022-04-13 23:51:17 +0200

    use htmlspecialchars and (int)

--- Views/admin/post/index.php
+++ Views/admin/post/index.php
@@ -18,12 +18,12 @@
     <tbody>
         <?php foreach ($params['posts'] as $post) : ?>
             <tr>
-                <th scope="row"><?= $post->id ?></th>
-                <td><?= $post->title ?></td>
+                <th scope="row"><?= (int)$post->id ?></th>
+                <td><?= htmlspecialchars($post->title) ?></td>
                 <td><?= $post->getCreatedAt() ?></td>
                 <td>
-                    <a href="/admin/posts/edit/<?= $post->id ?>" class="btn btn-warning">Modifier</a>
-                    <form action="/admin/posts/delete/<?= $post->id ?>" method="POST" class="d-inline">
+                    <a href="/admin/posts/edit/<?= (int)$post->id ?>" class="btn btn-warning">Modifier</a>
+                    <form action="/admin/posts/delete/<?= (int)$post->id ?>" method="POST" class="d-inline">
                         <button type="submit" class="btn btn-danger">Supprimer</button>
                     </form>
                 </td>

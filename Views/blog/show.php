3035cb53596b710e813b2386b0c32fb0a8144abd
Monky23 <lapaixjeanmichel@gmail.com>
2022-04-13 23:35:18 +0200

    use htmlspecialchars and nl2br

--- Views/blog/show.php
+++ Views/blog/show.php
@@ -1,8 +1,8 @@
-<h1><?= $params['post']->title ?></h1>
+<h1><?= htmlspecialchars($params['post']->title) ?></h1>
 <div>
     <?php foreach ($params['post']->getTags() as $tag) : ?>
-        <span class="badge bg-info"><a href="/tags/<?= $tag->id ?>" class="text-white"><?= $tag->name ?></a></span>
+        <span class="badge bg-info"><a href="/tags/<?= $tag->id ?>" class="text-white"><?= htmlspecialchars($tag->name)?></a></span>
     <?php endforeach ?>
 </div>
-<p><?= $params['post']->content ?></p>
+<p><?= nl2br(htmlspecialchars($params['post']->content)) ?></p>
 <a href="/posts" class="btn btn-secondary">Retourner en arrière</a>

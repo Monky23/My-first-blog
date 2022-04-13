479e1689612df66330188ff24a16a5a32d97806c
Monky23 <lapaixjeanmichel@gmail.com>
2022-04-13 21:41:52 +0200

    delete isset function

--- Views/admin/post/Form.php
+++ Views/admin/post/Form.php
@@ -1,13 +1,13 @@
-<h1><?= isset($params['post']->title) ?? 'Créer un nouvel article' ?></h1>
+<h1><?= $params['post']->title ?? 'Créer un nouvel article' ?></h1>
 
 <form action="<?= isset($params['post']) ? "/admin/posts/edit/{$params['post']->id}" : "/admin/posts/create" ?>" method="POST">
     <div class="form-group">
         <label for="title">Titre de l'article</label>
-        <input type="text" class="form-control" name="title" id="title" value="<?= isset($params['post']->title) ?? '' ?>">
+        <input type="text" class="form-control" name="title" id="title" value="<?= $params['post']->title ?? '' ?>">
     </div>
     <div class="form-group">
         <label for="content">Contenu de l'article</label>
-        <textarea name="content" id="content" rows="8" class="form-control"><?= isset($params['post']->content) ?? '' ?></textarea>
+        <textarea name="content" id="content" rows="8" class="form-control"><?= $params['post']->content ?? '' ?></textarea>
     </div>
     <div class="form-group">
         <label for="tags">Tags de l'article</label>

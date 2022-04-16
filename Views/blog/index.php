3d5293e214851528a2f4c887ca6ae5d24d3b4403
Monky23 <lapaixjeanmichel@gmail.com>
2022-04-16 19:24:09 +0200

    indent blog.index

--- Views/blog/index.php
+++ Views/blog/index.php
@@ -5,17 +5,27 @@
         <div class="card mb-3">
             <div class="row g-0">
                 <div class="col-md-4 mb-3 mt-3">
-                    <img src="../img/photoperso.jpg" class="img-fluid rounded-start" alt="">
+                    <img src="../img/photoperso.jpg" 
+                            class="img-fluid rounded-start" 
+                            alt="">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h2 class="card-title"><?= htmlspecialchars($post->title) ?></h2>
+                        <h2 class="card-title">
+                            <?= htmlspecialchars($post->title) ?>
+                        </h2>
                         <div>
                             <?php foreach ($post->getTags() as $tag) : ?>
-                                <span class="badge bg-info"><a href="/tags/<?= (int)$tag->id ?>" class="text-white"><?= htmlspecialchars($tag->name) ?></a></span>
+                                <span class="badge bg-info">
+                                    <a href="/tags/<?= (int)$tag->id ?>" class="text-white">
+                                        <?= htmlspecialchars($tag->name) ?>
+                                    </a>
+                                </span>
                             <?php endforeach ?>
                         </div>
-                        <small class="text-info">Publié le <?= $post->getCreatedAt() ?></small>
+                        <small class="text-info">
+                            Publié le <?= $post->getCreatedAt() ?>
+                        </small>
                         <p class="card-text"><?= $post->getExcerpt() ?></p>
                         <?= $post->getButton() ?>
                     </div>

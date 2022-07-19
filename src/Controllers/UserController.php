409ddc82bd70fd7625377fb6c670728cf99bd36a
Monky23 <lapaixjeanmichel@gmail.com>
2022-07-19 15:50:27 +0200

    delete unused comments

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -29,8 +29,6 @@ class UserController extends Controller
         }
 
         $user = (new User($this->getDB()))->getByUsername($_POST['username']);
-        //if (!isset($user))
-        //throw new Exception("cet identifiant est inconnu");
 
         if (password_verify($_POST['password'], $user->password)) {
             if ($_SESSION['auth'] = (int) $user->admin) {
@@ -55,40 +53,6 @@ class UserController extends Controller
         return $this->view('subscriber.registration');
     }
 
-    /*private function _checkAndSanitizeStr(string $str, int $minLen = 3): string
-    {
-        if (empty($str) || strlen($str) < $minLen || is_string($str))
-            throw new Exception("Vérifier les données");
-        return htmlspecialchars($str);
-    }*/
-
-    /*private function prepareDataForUserCreation(): array
-    {
-
-        $dto = [];
-        foreach ($_POST as $key => $value)
-            $dto[$key] = $this->_checkAndSanitizeStr($value);
-
-        if ($dto["password"] !== $dto["password_retype"])
-            throw new Exception("Les deux mots de passes doivent être identiques");
-        $dto["password"] = password_hash($dto["password_retype"], PASSWORD_ARGON2I);
-        unset($dto["password_retype"]);
-
-        if (!filter_var($dto["email"], FILTER_VALIDATE_EMAIL))
-            throw new Exception("Veuillez saisir une adresse mail valide");
-
-        /*if ($this->userExists($dto["email"]))
-            throw new Exception("Cette adresse mail est déjà utlisée pour un compte existant");*/
-
-    /*
-        [
-            "username" => "nom",
-            "email" => "email",
-            "password" => "Hashedpassword"
-        ]
-        
-        return $dto;
-    }*/
 
     public function registrationPost()
     {
